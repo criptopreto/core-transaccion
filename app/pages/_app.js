8b3522ff9331746b69c11e7e6a5acd9106a97dc3
Criptopreto <criptopreto@gmail.com>
2022-07-14 10:59:47 -0400

    Fix _app crossOrigin

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -41,7 +41,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
             content="black-translucent"
           ></meta>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;600;700&display=swap"
             rel="stylesheet"
